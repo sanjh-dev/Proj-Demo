afe7c53051c7e88f8001aa80a69928ea570492ba
sandhyagit19 <HP@LAPTOP-MTBBDEC7>
2020-02-22 16:10:01 +0530

    initial commit

---
+++ src/main/java/in/nit/model/User.java
@@ -0,0 +1,38 @@
+package in.nit.model;
+
+public class User {
+	private Integer id;
+	private String name;
+
+	public User(Integer id) {
+		super();
+		this.id = id;
+	}
+
+	public User() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	@Override
+	public String toString() {
+		return "User [id=" + id + ", name=" + name + "]";
+	}
+
+}
